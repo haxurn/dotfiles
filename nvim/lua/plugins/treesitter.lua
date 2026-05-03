13b0ca43dcc6f4a501ee9d27e58ba086117b27a7
Samson Tesfaye <haxurn@gmail.com>
2026-05-04 00:43:15 +0300

    fix: configure treesitter with vim-plug

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,33 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function ()
-    local treesitter = require("nvim-treesitter")
-    treesitter.setup()
-
-treesitter.install = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
-
-      callback = function()
-        vim.treesitter.start()
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end
+local languages = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "go",
+  "html",
+  "java",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "rust",
+  "tsx",
+  "typescript",
 }
+
+local treesitter = require("nvim-treesitter")
+treesitter.setup()
+
+vim.api.nvim_create_user_command("TSInstallConfigured", function()
+  treesitter.install(languages, { summary = true })
+end, {})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = languages,
+  callback = function()
+    pcall(vim.treesitter.start)
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
